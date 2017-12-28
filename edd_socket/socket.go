108f419c8d27ac964622840779012c25fb8b530d
Eddie Chan <dcwen199366@gmail.com>
2017-12-28 13:26:32 +0800

    Update socket.go
    add group func

--- edd_socket/socket.go
+++ edd_socket/socket.go
@@ -22,16 +22,18 @@ type Message struct {
 }
 
 //用户体
-type user struct {
-	uid string
+type User struct {
+	Uid  string
 	conn *websocket.Conn
-	name string
+	Name string
 }
 
 var (
-	member   =make(map[string]*user)
+	member   =make(map[string]*User)
 	uidMapWs =make(map[string]*websocket.Conn)
+	groupMapMember=make(map[string][]*User)
 )
+
 
 //实例化ws
 func NewWS(wss *websocket.Conn) *ws{
@@ -40,14 +42,26 @@ func NewWS(wss *websocket.Conn) *ws{
 
 //绑定uid
 func (this *ws) BindUid(uid,name string) {
-	client:=user{uid:uid,name:name,conn:this.coon}
+	client:= User{Uid:uid, Name:name,conn:this.coon}
 
 	member[uid]=&client
 	uidMapWs[uid]=this.coon
 }
 
+//是否在线
+func (this *ws) IsOnline(uid string) bool {
+	_,exits:=member[uid]
+	return exits
+}
+
 //断开连接
 func (this *ws) CloseUid(uid string) {
+	msg := Message{
+		Data: "离开房间",
+		Type: "del_user",
+	}
+	this.SendToAll(msg)
+
 	delete(member,uid)
 	delete(uidMapWs,uid)
 	this.coon.Close()
@@ -70,6 +84,49 @@ func (this *ws) SendToAll(msg Message) {
 		}
 	}
 }
+
+//获取当前组人数
+func (this *ws) GetClientCountByGroup(groupName string)int  {
+	return len(groupMapMember[groupName])
+}
+
+func (this *ws)GetClientByGroup(groupName string)[]*User {
+	return groupMapMember[groupName]
+}
+
+//加入某个群
+func (this *ws) JoinGroup(groupName,uid string)  {
+	groupMapMember[groupName]=append(groupMapMember[groupName],member[uid])
+}
+
+//给指定群发消息
+func (this *ws) SendToGroup(groupName string, msg Message) {
+	msg.TimeStamp=time.Now().Unix()
+	sendMess,_:=json.Marshal(msg)
+
+	for k,v:=range groupMapMember[groupName] {
+		if v.conn!=this.coon{
+			if err:=websocket.Message.Send(v.conn,string(sendMess));err!= nil {
+				//如果发送断裂，则该socket掉线
+				//删除当前组下面的切面中的元素即成员
+				kk:=k+ 1
+				groupMapMember[groupName]=append(groupMapMember[groupName][:k],groupMapMember[groupName][kk:]...)
+				continue
+			}
+		}
+	}
+}
+
+//离开某个群
+func (this *ws) LeaveGroup(groupName,uid string)  {
+	for k,v:=range groupMapMember[groupName] {
+		if v.Uid ==uid{
+				kk:=k+ 1
+				groupMapMember[groupName]=append(groupMapMember[groupName][:k],groupMapMember[groupName][kk:] ...)
+				break
+			}
+		}
+	}
 
 //发送给指定uid
 func (this *ws) SendToUid(uid string,msg Message) {
