a4ead9fa869e8a2f09a83d2a5fb5842e515704e8
Eddie <dcwen199366@gmail.com>
2018-01-10 16:17:55 +0800

    sun优化

--- edd_sun/sun_test.go
+++ edd_sun/sun_test.go
@@ -2,47 +2,12 @@ package edd_sun
 
 import (
 	"testing"
-	."eddie-handy/edd_log"
-	."eddie-handy/edd_curl"
-	sun "eddie-handy/edd_sun"
 	"fmt"
 )
 
 func TestSun(t *testing.T) {
-	sunObj :=sun.NewObject(13.2,23.1,(+8))
-	sunT,_ :=sunObj.GetSunTime(sun.SUN_SET)
+	sunObj :=NewObject(13.2,23.1,(+8))
+	sunT,_ :=sunObj.GetSunTime(SUN_SET)
 	fmt.Println(sunT)
 
-}
-
-func TestLog(t *testing.T) {
-	Config("one.log")
-	Debugf("Debugf")
-	Infof("Infof")
-	Warnf("Warnf")
-	Errorf("Errorf")
-	Fatalf("Fatalf")
-
-	aa:=LogFile{}
-	aa.Write([]byte("what fox\n"))
-}
-
-func TestCurl(t *testing.T) {
-	url:= "http://sms-api.luosimao.com/v1/send.json"
-
-	headers:=map[string]string{
-		"Data-Type":"application/x-www-form-urlencoded",
-		"Authorization":BasicAuth("api","78aac6166f23182bd2eaceae0fba6aa84"),
-	}
-	postData:=map[string]string{
-		"mobile":"18380591566",
-		"message":"go-lang test【环球娃娃】",
-	}
-
-	result:=NewRequst(url).
-		SetHeaders(headers).
-		SetPostData(postData).
-		Post()
-
-	fmt.Println(result)
 }
